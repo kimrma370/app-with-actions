f3e0a9337b9d1b2966767e952851d9e8b336d5d6
kimrma370 <kimrma370@gmail.com>
2025-06-02 10:08:26 +0900

    update controller

--- src/main/java/actions/controller/ActionsController.java
+++ src/main/java/actions/controller/ActionsController.java
@@ -6,9 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ActionsController {
 	
+	@GetMapping(value = "/")
+	public String basicTest() {
+		return "app-with-actions";
+	}
+	
 	@GetMapping(value = "/api/actions")
 	public String actionsTest() {
-		return "app-with-actions version:0.0.2";
+		return "app-with-actions version:0.0.3";
 	}
 	
 }
